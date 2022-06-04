ddcf0caedee7621b11df07e9868b387adf958710
ClaudioPM22 <cpinillam2340@gmail.com>
2022-06-04 16:22:41 -0400

    recorriendo string

--- Tarea.py
+++ Tarea.py
@@ -1,6 +1,7 @@
  #Implementación de una pila en pyhton
  
 from inspect import stack
+from xmlrpc.client import boolean
 
 
 class Stack: # Creamos la clase Stack
@@ -23,11 +24,21 @@ class Stack: # Creamos la clase Stack
 pila = Stack()
 
 # ingresamos algunos elementos a la pila
-pila.push('a')
-pila.push('b')
-pila.push('c')
-  
-pila.print_stack() # Mostramos los elementos de la pila
-  
-pila.pop() # Utilizamos el metodo pop
-pila.print_stack() # Mostramos nuevamente los elementos de la pila
+#pila.push('a')
+#pila.push('b')
+#pila.push('c') 
+#pila.print_stack() # Mostramos los elementos de la pila
+#pila.pop() # Utilizamos el metodo pop
+#pila.print_stack() # Mostramos nuevamente los elementos de la pila
+
+q0 = q1 = q2 = q3 = q4 = q5 = True 
+estados = [q0,q1,q2,q3,q4,q5]
+estadoInicial = [q0]
+estadoFinal = []
+
+palabra = input("Ingrese palabra de entrada: ")
+
+for i in palabra:
+    print(i, end = "")
+print("\n")
+
